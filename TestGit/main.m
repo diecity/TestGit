2b3fb73280010cc27c4dbdde6d47f26e014ba5d5
diecity <diecity@163.com>
2014-04-01 16:47:15 +0800

    Initial Commit

---
+++ TestGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by teelab1 on 14-4-1.
+//  Copyright (c) 2014年 TestGit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
